5823d0a48524acbffe835aee137af1d537f9ae25
Torstein Vik <torsteinv64@gmail.com>
2017-10-17 00:42:25 +0200

    Added rough draft of DefaultEventBus

--- src/main/scala/api/event/EventBus.scala
+++ src/main/scala/api/event/EventBus.scala
@@ -1,7 +1,27 @@
 package io.zophie.api.event
 
-trait EventBus {
+sealed trait EventBus {
     def addEventListener(event : Event) : ((event.eventData => Unit) => Unit)
     def hasEventListener(event : Event) : Boolean
-    def triggerEvent(event : Event) : ((event.eventData => Unit) => Unit)
+    def triggerEvent(event : Event) : (event.eventData => Unit)
+}
+
+class DefaultEventBus extends EventBus {
+
+    override def addEventListener(event : Event) : ((event.eventData => Unit) => Unit) = {
+        return (f : (event.eventData => Unit)) => {
+
+        }
+    }
+
+    override def hasEventListener(event : Event) : Boolean = {
+        return false
+    }
+
+    override def triggerEvent(event : Event) : (event.eventData => Unit) = {
+        return (data : event.eventData) => {
+
+        }
+    }
+
 }
