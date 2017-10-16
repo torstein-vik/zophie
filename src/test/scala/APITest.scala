720e20cfb742bcd74a3ab09bb3a8c2e0e40c369d
Torstein Vik <torsteinv64@gmail.com>
2017-10-17 01:37:53 +0200

    Added test for hasEventListener and addEventListener

--- src/test/scala/APITest.scala
+++ src/test/scala/APITest.scala
@@ -21,4 +21,24 @@ class APITest extends FunSuite {
         assert(!eventbus.hasEventListener(Event2))
 
     }
+
+    test ("eventbus.hasEventListener and addEventListener works") {
+
+        // The eventbus which is to be tested
+        val eventbus : EventBus = new DefaultEventBus()
+
+        // Add event listener for Event1
+        eventbus.addEventListener(Event1)(new EventHandler [EventData1] { def handle ( data : EventData1 ) { } });
+
+        // Make sure it has Event1, but not Event2
+        assert(eventbus.hasEventListener(Event1))
+        assert(!eventbus.hasEventListener(Event2))
+
+        // Add event listener for Event2
+        eventbus.addEventListener(Event1)(new EventHandler [EventData1] { def handle ( data : EventData1 ) { } });
+
+        // Make sure it has all eventListeners
+        assert(eventbus.hasEventListener(Event1))
+        assert(eventbus.hasEventListener(Event2))
+    }
 }
