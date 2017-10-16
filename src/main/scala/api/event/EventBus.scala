9cf45a0a7dda9156e34bc8c901586059ab894f54
Torstein Vik <torsteinv64@gmail.com>
2017-10-17 00:23:05 +0200

    Added 3 basic event bus methods to EventBus trait

--- src/main/scala/api/event/EventBus.scala
+++ src/main/scala/api/event/EventBus.scala
@@ -1,5 +1,7 @@
-package io.zophie.api
+package io.zophie.api.event
 
 trait EventBus {
-
+    def addEventListener(event : Event) : ((event.eventData => Unit) => Unit)
+    def hasEventListener(event : Event) : Boolean
+    def triggerEvent(event : Event) : ((event.eventData => Unit) => Unit)
 }
