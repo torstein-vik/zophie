2dd3c6a0e2b6870537a15022a48ba7334b71435a
Torstein Vik <torsteinv64@gmail.com>
2017-10-17 00:49:13 +0200

    Removed metatest, Added definitions for EventBus test

--- src/test/scala/ZophieTest.scala
+++ src/test/scala/ZophieTest.scala
@@ -1,8 +1,21 @@
 import org.scalatest.FunSuite
 
-class ZophieTest extends FunSuite {
-    test("metatest") {
-        assert(true)
-    }
+import io.zophie.api.event._
 
+class ZophieTest extends FunSuite {
+    test ("eventbus") {
+
+        // Defining two dummy events and event data classes
+        class EventData1 (str : String) extends EventData
+        class EventData2 (num : Int)    extends EventData
+
+        case object Event1 extends Event {type eventData = EventData1}
+        case object Event2 extends Event {type eventData = EventData2}
+
+        // The eventbus which is to be tested
+        val eventbus : EventBus = new DefaultEventBus()
+
+
+
+    }
 }
