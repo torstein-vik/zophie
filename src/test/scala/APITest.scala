51557b897a0aacb49f56cfedf166bcc9276e0c3e
Torstein Vik <torsteinv64@gmail.com>
2017-10-17 01:07:18 +0200

    Restructured testing

--- src/test/scala/APITest.scala
+++ src/test/scala/APITest.scala
@@ -3,13 +3,15 @@ import org.scalatest.FunSuite
 import io.zophie.api.event._
 
 class APITest extends FunSuite {
-    test ("eventbus") {
 
-        // Defining two dummy events and event data classes
-        class EventData1 (str : String) extends EventData
+    // Defining two dummy events and event data classes
+    class EventData1 (str : String) extends EventData
 
-        case object Event1 extends Event {type eventData = EventData1}
-        case object Event2 extends Event {type eventData = NoEventData}
+    case object Event1 extends Event {type eventData = EventData1}
+    case object Event2 extends Event {type eventData = NoEventData}
+
+
+    test ("eventbus does not have event listeners") {
 
         // The eventbus which is to be tested
         val eventbus : EventBus = new DefaultEventBus()
