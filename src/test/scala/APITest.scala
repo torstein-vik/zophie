0c6c24b252b53a780b80a704a9292f4c7f4766e4
Torstein Vik <torsteinv64@gmail.com>
2017-10-17 23:23:08 +0200

    Renamed APITest to EventBusTest

--- src/test/scala/APITest.scala
+++ src/test/scala/APITest.scala
@@ -6,7 +6,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 import io.zophie.api.event._
 
-class APITest extends FunSuite with ScalaFutures {
+class EventBusTest extends FunSuite with ScalaFutures {
 
     // Defining two dummy events and event data classes
     class EventData1 (val str : String) extends EventData
