c38cf27ed41f44b0989c291322abfeb3d7f77333
Torstein Vik <torsteinv64@gmail.com>
2017-10-17 23:28:37 +0200

    Updated definitions of EventBus functions

--- src/main/scala/api/event/EventBus.scala
+++ src/main/scala/api/event/EventBus.scala
@@ -6,28 +6,26 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 trait EventBus {
-    def addEventListener(event : Event) : (EventHandler[event.eventData] => Unit)
+    def addEventListener(event : Event)(handler : EventHandler[event.eventData]) : Unit
     def hasEventListener(event : Event) : Boolean
-    def triggerEvent(event : Event) : (event.eventData => Future[Unit])
-    def triggerEventSync(event : Event) : (event.eventData => Unit)
+    def triggerEvent(event : Event)(data : event.eventData) : Future[Unit]
+    def triggerEventSync(event : Event)(data : event.eventData) : Unit
 }
 
 class DefaultEventBus extends EventBus {
 
     private var listeners : Map[Event, Seq[EventHandler[_]]] = Map()
 
-    override def addEventListener(event : Event) : (EventHandler[event.eventData] => Unit) = {
-        return (handler : EventHandler[event.eventData]) => {
-            listeners += event -> (listeners.getOrElse(event, Seq()) ++ Seq(handler))
-        }
+    override def addEventListener(event : Event)(handler : EventHandler[event.eventData]) = {
+        listeners += event -> (listeners.getOrElse(event, Seq()) ++ Seq(handler))
     }
 
     override def hasEventListener(event : Event) : Boolean = {
         return listeners.getOrElse(event, Seq()).length > 0
     }
 
-    override def triggerEvent(event : Event) : (event.eventData => Future[Unit]) = {
-        return (data : event.eventData) => Future.sequence(
+    override def triggerEvent(event : Event)(data : event.eventData) : Future[Unit] = {
+        return Future.sequence(
             listeners.getOrElse(event, Seq()).map(handler =>
                 Future [Unit] {
                     handler.asInstanceOf[EventHandler[event.eventData]].handle(data)
@@ -36,10 +34,8 @@ class DefaultEventBus extends EventBus {
         ).map( _ => Unit )
     }
 
-    override def triggerEventSync(event : Event) : (event.eventData => Unit) = {
-        return (data : event.eventData) => {
-            listeners.getOrElse(event, Seq()).foreach(_.asInstanceOf[EventHandler[event.eventData]].handle(data))
-        }
+    override def triggerEventSync(event : Event)(data : event.eventData) = {
+        listeners.getOrElse(event, Seq()).foreach(_.asInstanceOf[EventHandler[event.eventData]].handle(data))
     }
 
 }
