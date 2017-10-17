785a9925053f3b71226762b539a098add93e2df8
Torstein Vik <torsteinv64@gmail.com>
2017-10-17 23:56:46 +0200

    Added comments to Events.scala

--- src/main/scala/api/event/Events.scala
+++ src/main/scala/api/event/Events.scala
@@ -1,13 +1,17 @@
 package io.zophie.api.event
 
+// An event is something that an EventBus can invoke. Should be defined using case object, to be used as an Enum. Includes the type of the data which is passed alongside it
 trait Event {
     type eventData <: EventData
 }
 
+// An EventHandler takes the eventData and performs some task. Takes the type of the EventData as a type parameter
 trait EventHandler[T <: EventData] {
     def handle (data : T) : Unit
 }
 
+// Datacontainer which may be passed through an EventBus
 trait EventData
 
+// Specific case of no event data
 case object NoEventData extends EventData
