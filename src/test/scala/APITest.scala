1b40fd24435203c19d54494115dec86064a8ac4f
Torstein Vik <torsteinv64@gmail.com>
2017-10-17 20:42:31 +0200

    Made str in EventData1 val, so it can be accessible

--- src/test/scala/APITest.scala
+++ src/test/scala/APITest.scala
@@ -5,7 +5,7 @@ import io.zophie.api.event._
 class APITest extends FunSuite {
 
     // Defining two dummy events and event data classes
-    class EventData1 (str : String) extends EventData
+    class EventData1 (val str : String) extends EventData
 
     case object Event1 extends Event {type eventData = EventData1}
     case object Event2 extends Event {type eventData = NoEventData}
