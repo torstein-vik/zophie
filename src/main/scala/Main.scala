e7732b7f7cd35c5cbd844b4365d06df34405f6d2
torstein-vik <torsteinv64@gmail.com>
2017-10-20 17:34:48 +0200

    Added some comments to Main.scala and API.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,7 @@ package io.zophie
 import api.event._
 import api._
 
+// Main object, performs all backend interfacing with the IO part of the architecture
 object Main extends API {
 
 }
--- src/main/scala/api/API.scala
+++ src/main/scala/api/API.scala
@@ -2,7 +2,11 @@ package io.zophie.api
 
 import event._
 
+
+// Class for Main to extend, some shorthands for nicer code
 class API extends DefaultEventBus {
+
+    // Shorthands for addEventHandler and triggerEvent
     def trigger(event : Event)(implicit data : event.eventData) = triggerEvent(event)(data)
     def on(event : Event)(handler : EventHandler[event.eventData]) = addEventHandler(event)(handler)
 
