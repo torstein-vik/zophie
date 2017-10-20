f4f7b46263e05b612c0c0ed3edbc99b353db3933
torstein-vik <torsteinv64@gmail.com>
2017-10-20 12:02:02 +0200

    Added test for API.scala, based on eventbus test

--- src/test/scala/APITest.scala
+++ src/test/scala/APITest.scala
@@ -4,18 +4,19 @@ import org.scalatest.concurrent.ScalaFutures
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
+import io.zophie.api._
 import io.zophie.api.event._
 
+
+// Defining two dummy events and event data classes
+class EventData1 (val str : String) extends EventData
+class EventData2 (val num : Int)    extends EventData
+
+case object Event1 extends Event {type eventData = EventData1}
+case object Event2 extends Event {type eventData = NoEventData.type}
+case object Event3 extends Event {type eventData = EventData2}
+
 class EventBusTest extends FunSuite with ScalaFutures {
-
-    // Defining two dummy events and event data classes
-    class EventData1 (val str : String) extends EventData
-    class EventData2 (val num : Int)    extends EventData
-
-    case object Event1 extends Event {type eventData = EventData1}
-    case object Event2 extends Event {type eventData = NoEventData.type}
-    case object Event3 extends Event {type eventData = EventData2}
-
 
     test ("empty eventbus does not have event handlers") {
 
@@ -229,6 +230,108 @@ class EventBusTest extends FunSuite with ScalaFutures {
         // Testing events
         // Note that there is no order that these should be executed in, as they are execured in parallell
         whenReady(eventbus.triggerEvent(Event2)(NoEventData)) {
+            _ => assert(x === 1 || x === 2 || x === 3)
+        }
+    }
+}
+
+class APITest extends FunSuite with ScalaFutures {
+
+
+    test ("API trigger works") {
+
+        // The API which is to be tested
+        val api : API = new API()
+
+        // Variable to be changed by event
+        var x = "test1"
+
+        // A handler on Event1, which changes x to event.str
+        api.on(Event1)(event => x = event.str)
+
+        // Nothing happends when Event2 is triggered
+        val eventTrigger1 = api.trigger(Event2)(NoEventData)
+        whenReady(eventTrigger1) {
+            case _ => assert(x === "test1")
+        }
+
+
+        // x changes to 'test2' when Event1 triggered with this data
+        val eventTrigger2 = api.trigger(Event1)(new EventData1("test2"))
+        whenReady(eventTrigger2) {
+            case _ => assert(x === "test2")
+        }
+
+
+        // x changes to 'test3' when Event1 triggered with this data
+        val eventTrigger3 = api.trigger(Event1)(new EventData1("test3"))
+        whenReady(eventTrigger3) {
+            case _ => assert(x === "test3")
+        }
+
+
+        // Nothing happends when Event2 is triggered
+        val eventTrigger4 = api.trigger(Event2)(NoEventData)
+        whenReady(eventTrigger4) {
+            case _ => assert(x === "test3")
+        }
+
+
+    }
+
+    test ("API multiple events") {
+
+        // The API which is to be tested
+        val api : API = new API()
+
+        // Variables to be tested
+        var x = 0
+        var y = 0
+
+        // Eventhandlers
+        api.on(Event2)(_ => x += 1)
+        api.on(Event2)(_ => y += 2)
+        api.on(Event3)(data => x = data.num * 2 )
+        api.on(Event3)(data => y = data.num )
+
+
+        // Testing events
+        whenReady(api.trigger(Event2)(NoEventData)) {
+            _ => assert(x === 1 && y === 2)
+        }
+
+        whenReady(api.trigger(Event2)(NoEventData)) {
+            _ => assert(x === 2 && y === 4)
+        }
+
+        whenReady(api.trigger(Event3)(new EventData2(num = 3))) {
+            _ => assert(x === 6 && y === 3)
+        }
+
+        whenReady(api.trigger(Event2)(NoEventData)) {
+            _ => assert(x === 7 && y === 5)
+        }
+
+        whenReady(api.trigger(Event3)(new EventData2(num = 4))) {
+            _ => assert(x === 8 && y === 4)
+        }
+    }
+
+    test ("API multiple event order") {
+        // The API which is to be tested
+        val api : API = new API()
+
+        // Variables to be tested
+        var x = 0
+
+        // Eventhandlers
+        api.on(Event2)(_ => x = 1)
+        api.on(Event2)(_ => x = 2)
+        api.on(Event2)(_ => x = 3)
+
+        // Testing events
+        // Note that there is no order that these should be executed in, as they are execured in parallell
+        whenReady(api.trigger(Event2)(NoEventData)) {
             _ => assert(x === 1 || x === 2 || x === 3)
         }
     }
