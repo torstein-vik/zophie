5e6290f2d6baaf774d9b7b6cc8b886bf44f195e5
torstein-vik <torsteinv64@gmail.com>
2017-10-20 11:49:46 +0200

    Added 'trigger' and 'on' to API

--- src/main/scala/api/API.scala
+++ src/main/scala/api/API.scala
@@ -3,5 +3,6 @@ package io.zophie.api
 import event._
 
 class API extends DefaultEventBus {
-
+    def trigger(event : Event)(data : event.eventData) = triggerEvent(event)(data)
+    def on(event : Event)(handler : EventHandler[event.eventData]) = addEventHandler(event)(handler)
 }
