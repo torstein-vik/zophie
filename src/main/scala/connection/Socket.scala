07dbf2d56f78f2fffd9ced615d34dcdc7c538341
torstein-vik <torsteinv64@gmail.com>
2017-10-21 09:43:44 +0200

    Implemented SocketConnection

--- src/main/scala/connection/Socket.scala
+++ src/main/scala/connection/Socket.scala
@@ -1,11 +1,34 @@
 package io.zophie.connection
 
-class Socket (implicit callback : ConnectionCallback[String]) extends Connection[String](callback) {
+import java.net._
+import java.io._
+import scala.io._
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
+class SocketConnection (ip : InetAddress, port : Int)(implicit callback : ConnectionCallback[String]) extends Connection[String](callback) {
+
+    val socket = new Socket(ip, port)
+
+    lazy val input = new BufferedSource( socket.getInputStream ).getLines
+    val output = new PrintStream( socket.getOutputStream )
+
+    var running = true;
+
+    def close () = {
+        running = false;
+        socket.close();
+    }
+
+    Future {
+        while(running) {
+            callback.callback(input.next)
+        }
+    }
 
     override def push (data : String) = {
-
+        output.println(data)
     }
 }
 
