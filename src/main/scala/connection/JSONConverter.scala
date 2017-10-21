211e6b409d9abea706d797671cb1193a486a7cdf
torstein-vik <torsteinv64@gmail.com>
2017-10-21 12:44:11 +0200

    Added placeholder functionality to JSONConverter

--- src/main/scala/connection/JSONConverter.scala
+++ src/main/scala/connection/JSONConverter.scala
@@ -4,12 +4,15 @@ import io.zophie.api.event._
 
 package object JSONConverter {
     implicit object jsonc extends EventConverter[String] {
-        def toData   (event : EventDataComposite) : String = {
 
+        case object Placeholder extends Event  {type eventData = NoEventData.type}
+
+        def toData   (event : EventDataComposite) : String = {
+            return "placeholder"
         }
 
         def fromData (data  : String) : EventDataComposite = {
-
+            return new EventDataComposite(Placeholder)(NoEventData)
         }
     }
 }
