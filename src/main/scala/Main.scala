7302aee09f76405045cabbfa003377cf762878fd
torstein-vik <torsteinv64@gmail.com>
2017-10-21 11:37:44 +0200

    Redid Main class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,22 @@
 package io.zophie
 
+import java.net._
+
+import connection._
 import api.event._
 import api._
 
-// Main object, performs all backend interfacing with the IO part of the architecture
-object Main extends API {
+// Main object, performs all backend interfacing with the IO part of the architecture and is extended by Zophie object
+class Main[S, T] (connection : ConnectionFactory[S, T]) {
+
+
+
+    private object backend extends API {
+
+    }
+
+    private object io extends API {
+
+    }
 
 }
