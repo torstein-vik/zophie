d5d24c7084f473a9b119cb794f411ebfa9ecd92a
torstein-vik <torsteinv64@gmail.com>
2017-10-21 16:33:47 +0200

    Added comments to JSONConverter.scala

--- src/main/scala/connection/JSONConverter.scala
+++ src/main/scala/connection/JSONConverter.scala
@@ -2,16 +2,26 @@ package io.zophie.connection
 
 import io.zophie.api.event._
 
+// A converter that creates JSON-data from an Event-Event data pair
 package object JSONConverter {
+
+    // Implicit version, so it doesn't need to be passed
     implicit object jsonc extends EventConverter[String] {
 
+        // For now, just a placeholder event
         case object Placeholder extends EventNoData
 
+        // Stringifies the JSON for the pair
         def toData   (event : EventDataComposite) : String = {
+
+            // For now just returns placeholder
             return "placeholder"
         }
 
+        // Parses JSON string into Event-EventData pair
         def fromData (data  : String) : EventDataComposite = {
+
+            // Just return placeholder
             return new EventDataComposite(Placeholder)(NoEventData)
         }
     }
