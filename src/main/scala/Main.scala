18cb93de7c24a86ed581c0801feccaf8dd9154e2
torstein-vik <torsteinv64@gmail.com>
2017-10-21 12:17:20 +0200

    Added more connection infrastructure to Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,16 +7,31 @@ import api.event._
 import api._
 
 // Main object, performs all backend interfacing with the IO part of the architecture and is extended by Zophie object
-class Main[S, T <: Connection[S]] (connection : ConnectionFactory[S, T]) {
+class Main[S, T <: Connection[S]] (connectionFactory : ConnectionFactory[S, T]) {
 
+    val connection : T = connectionFactory.setupConnection(backend)
 
+    private object backend extends API with ConnectionCallback[S] {
 
-    private object backend extends API {
+        def callback (data : S) {
+
+        }
 
     }
 
     private object io extends API {
 
+        override def triggerEvent (event : Event)(implicit data : event.eventData) = {
+            super.triggerEvent(event)(data)
+        }
+
+        override def triggerEventSync (event : Event)(implicit data : event.eventData) = {
+            super.triggerEventSync(event)(data)
+        }
+
     }
+
+    def trigger(event : Event)(implicit data : event.eventData) = io.trigger(event)(data)
+    def on(event : Event)(handler : EventHandler[event.eventData]) = backend.on(event)(handler)
 
 }
