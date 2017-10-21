68081134b5f6e1b7d20053853fb8cbfe4b325a53
torstein-vik <torsteinv64@gmail.com>
2017-10-21 16:35:21 +0200

    Added comments to Zophie.scala

--- src/main/scala/Zophie.scala
+++ src/main/scala/Zophie.scala
@@ -6,6 +6,8 @@ import connection._
 import connection.JSONConverter._
 import DefaultNetworkDetails._
 
+// Primary object, where it all begins (extends App)
+// Also extends Main for API-access, with a string-based SocketConnection using setup from DefaultNetworkDetails
 object Zophie extends Main[String, SocketConnection](SocketConnection.setup) with App {
 
 }
