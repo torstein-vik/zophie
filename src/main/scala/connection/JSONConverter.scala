f4e4733b08fc9a094e01d3f4713d9eae71c53ca6
torstein-vik <torsteinv64@gmail.com>
2017-10-21 12:37:00 +0200

    Added to and from data to JSONConverter

--- src/main/scala/connection/JSONConverter.scala
+++ src/main/scala/connection/JSONConverter.scala
@@ -4,6 +4,12 @@ import io.zophie.api.event._
 
 package object JSONConverter {
     implicit object jsonc extends EventConverter[String] {
+        def toData   (event : EventDataComposite) : String = {
 
+        }
+
+        def fromData (data  : String) : EventDataComposite = {
+
+        }
     }
 }
