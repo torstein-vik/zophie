f6ffe89c806eae72a4fee18894b2c707b6a6fb42
torstein-vik <torsteinv64@gmail.com>
2017-10-28 15:42:14 +0200

    Added EventDataJSONConverter and implicit for NoEventData

--- src/main/scala/connection/JSONConverter.scala
+++ src/main/scala/connection/JSONConverter.scala
@@ -2,9 +2,22 @@ package io.zophie.connection
 
 import io.zophie.api.event._
 
+import org.json4s._
+
+// Implementation of JSON, to be passed to JSON converter 
+trait EventDataJSONConverter[T <: EventData] {
+    def toJSON   (data : T) : JObject
+    def fromJSON (data : JObject) : T
+}
+
 // A converter that creates JSON-data from an Event-Event data pair
 package object JSONConverter {
 
+    implicit object edjsonNoEventData extends EventDataJSONConverter[NoEventData.type] {
+        override def toJSON   (data : NoEventData.type) = JObject()
+        override def fromJSON (data : JObject) = NoEventData
+    }
+    
     // Implicit version, so it doesn't need to be passed
     implicit object jsonc extends EventConverter[String] {
 
@@ -12,7 +25,7 @@ package object JSONConverter {
         case object Placeholder extends EventNoData
 
         // Stringifies the JSON for the pair
-        def toData   (event : EventDataComposite) : String = {
+        def toData (event : EventDataComposite) : String = {
 
             // For now just returns placeholder
             return "placeholder"
