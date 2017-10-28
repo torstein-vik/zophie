74b4ee2ec4436076a2ae33f564d1a6282d9e9c73
Torstein Vik <torsteinv64@gmail.com>
2017-10-29 01:52:02 +0200

    Made EventDataComposite and Event covariant

--- src/main/scala/api/event/Events.scala
+++ src/main/scala/api/event/Events.scala
@@ -2,7 +2,7 @@ package io.zophie.api.event
 
 // An event is something that an EventBus can invoke. Should be defined using case object, to be used as an Enum. 
 // Includes the type of the data which is passed alongside it, and a name
-abstract class Event[T <: EventData](val name : String) {
+abstract class Event[+T <: EventData](val name : String) {
     override def toString = name
 }
 
@@ -18,8 +18,8 @@ trait EventHandler[T <: EventData] {
 trait EventData
 
 // Container for both the event and the data
-class EventDataComposite[T <: EventData] (val event : Event[T])(implicit val data : T) {
-    def ==(that : EventDataComposite[T]) = (event == that.event) && (data == that.data)
+class EventDataComposite[+T <: EventData] (val event : Event[T])(implicit val data : T) {
+    def ==(that : EventDataComposite[EventData]) = (event == that.event) && (data == that.data)
     
     override def toString = event + " with data: " + data
 }
