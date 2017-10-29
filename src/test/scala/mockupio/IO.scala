17517abc210260eaa6f15a132f3854e53e262c45
Torstein Vik <torsteinv64@gmail.com>
2017-10-29 21:15:36 +0100

    Added basic server code

--- src/test/scala/mockupio/IO.scala
+++ src/test/scala/mockupio/IO.scala
@@ -1,7 +1,34 @@
 package mockupio
 
+import java.net._
+import java.io._
+import scala.io._
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ Future, Promise }
+
 class IO (port : Int) {
     
-    def close = null
+    val server = new ServerSocket(port)
+    
+    var running = true
+    
+    Future {
+        val session = server.accept()
+        
+        val out = new PrintStream( session.getOutputStream )
+        val in  = new BufferedSource( session.getInputStream ).getLines
+        
+        while (running) {
+            
+        }
+        
+        session.close()
+    }
+    
+    def close = {
+        server.close()
+        running = false
+    }
     
 }
